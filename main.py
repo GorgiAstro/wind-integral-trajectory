a47cdfb84056739211910d67177dcef0a7322ccc
Clément Jonglez <clement.jonglez@tu-berlin.de>
2018-12-08 12:13:40 +0100

    Reads Wind position from CDF files

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 if __name__ == "__main__":
+    ## Parameters
+    from datetime import datetime, timezone
+    startDate = datetime(2012, 10, 1, tzinfo=timezone.utc)
+    endDate = datetime(2012, 11, 15, tzinfo=timezone.utc)
+
     ## Getting Integral position
     integralSpiceId = '-198'
     observer = '500' # Earth
     from astroquery.jplhorizons import Horizons
+    epochs = {'start': '{:4d}-{:02d}-{:02d}'.format(startDate.year, startDate.month, startDate.day),
+                             'stop': '{:4d}-{:02d}-{:02d}'.format(endDate.year, endDate.month, endDate.day),
+                             'step': '10m'}
     obj = Horizons(id=integralSpiceId,
                    location=observer,
-                   epochs={'start': '2012-10-01',
-                             'stop': '2012-10-14',
-                             'step':'10m'},
+                   epochs=epochs,
                    id_type='id')
     vec = obj.vectors()
     # By default, coordinates should be in J2000 ecliptic coordinates, but not sure...
@@ -20,22 +26,31 @@ if __name__ == "__main__":
     #print(integralDate_jd)
 
     ## Getting WIND position
-    windDownloadUrl = "http://spdf.gsfc.nasa.gov/pub/data/wind/orbit/pre_or/2012/wi_or_pre_20121001_v02.cdf"
-    windFileName = "wi_or_pre_20121001_v02.cdf"
-    import requests
-    r = requests.get(windDownloadUrl)
-    with open(windFileName, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=128):
-            f.write(chunk)
-
     import cdflib
-    cdfFile = cdflib.CDF(windFileName)
-    windPosition_km = cdfFile.varget(variable='GSE_POS')
-    # Getting position in Geocentral solar ecliptic (GSE) frame
-    # Other frames are available in the CDF file:
-    #   - Geocentric equatorial inertial (GCI)
-    #   - Geocentric solar magnetic (GSM)
-    windEpoch_PB5 = cdfFile.varget(variable='Time_PB5')
+    from pathlib import Path
+    windPosition_km = None
+    windEpoch_PB5 = None
+    p = Path('.')
+    for windFileName in sorted(p.glob('../../pre_or/*/*.cdf')):
+        fileNameWithoutPath = windFileName.name
+        fileYear = int(fileNameWithoutPath[10:14])
+        fileMonth = int(fileNameWithoutPath[14:16])
+        fileDay = int(fileNameWithoutPath[16:18])
+        fileDate = datetime(fileYear, fileMonth, fileDay, tzinfo=timezone.utc)
+        if (fileDate >= startDate) and (fileDate <= endDate):
+            print(fileDate)
+            cdfFile = cdflib.CDF(windFileName)
+            if windPosition_km is None:
+                windPosition_km = cdfFile.varget(variable='GCI_POS')
+                windEpoch_PB5 = cdfFile.varget(variable='Time_PB5')
+            else:
+                windPosition_km = np.concatenate((windPosition_km, cdfFile.varget(variable='GCI_POS')))
+                # Getting position in Geocentral solar ecliptic (GSE) frame
+                # Other frames are available in the CDF file:
+                #   - Geocentric equatorial inertial (GCI)
+                #   - Geocentric solar magnetic (GSM)
+                windEpoch_PB5 = np.concatenate((windEpoch_PB5, cdfFile.varget(variable='Time_PB5')))
+
     #print(windPosition_km)
     #print(windEpoch_PB5)
 
